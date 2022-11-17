2d48649ff80a17ec09d3e6c1deb7e09830bb17c8
HadrianDeveloper <hadrian.developer@outlook.com>
2022-11-17 17:17:43 +0000

    #9 GET api/users built and tested with 404 and 200 capabilities

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require('express');
-const app = express()
+const app = express();
+app.use(express.json());
 
 const { getAllTopics } = require('./controllers/topics-controller.js');
 const { getAllArticles, getArticleById, getCommentsForArticle, postComment, patchArticle } = require('./controllers/articles-controller.js');
+const { getAllUsers } = require('./controllers/users-controller.js');
 const { handle404s, handlePSQLerrors, handleCustomErrors } = require('./errors/errors.js');
-app.use(express.json());
 
 app.get('/api/topics', getAllTopics);
 app.get('/api/articles', getAllArticles);
 app.get('/api/articles/:article_id', getArticleById);
 app.get('/api/articles/:article_id/comments', getCommentsForArticle)
+app.get('/api/users', getAllUsers)
 
 app.post('/api/articles/:article_id/comments', postComment)
 app.patch('/api/articles/:article_id', patchArticle)
